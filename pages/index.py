4013985f789c8499d325790e74f85e391cb6ac9b
Jessica Williams <32785700+JayellWolfe@users.noreply.github.com>
2019-09-22 16:19:41 -0400

    Update index.py

--- pages/index.py
+++ pages/index.py
@@ -9,15 +9,11 @@ from app import app
 
 """
 https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
-
 Layout in Bootstrap is controlled using the grid system. The Bootstrap grid has 
 twelve columns.
-
 There are three main layout components in dash-bootstrap-components: Container, 
 Row, and Col.
-
 The layout of your app should be built as a series of rows of columns.
-
 We set md=4 indicating that on a 'medium' sized or larger screen each column 
 should take up a third of the width. Since we don't specify behaviour on 
 smaller size screens Bootstrap will allow the rows to wrap so as not to squash 
@@ -28,14 +24,23 @@ column1 = dbc.Col(
     [
         dcc.Markdown(
             """
+            ## Predicting Earthquakes 
+            
+            Earthquakes have devastatingly deadly consequences. The ability to predict earthquakes
+            is one of the most important challenges facing Earth Science today. And being able to
+            predict and forecast earthquakes before they occur would mean saving countless lives
+            and billions of dollars in infrastucture
+            
+            Three focus key points are when the event will occur, where it will occur, and how 
+            large it will be.
+
+            Using real-time siesmic signal data in a laboratory setting, this will focus on the 
+            when, predicting the time remaining until the next earthquake. 
+
         
-            ## Predictions
-
-            Predict the time to failure during earthquake simulations
-
             """
         ),
-        dcc.Link(dbc.Button('Call To Action', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('Predictions', color='primary'), href='/predictions')
     ],
     md=4,
 )
